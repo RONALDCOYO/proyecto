9d8311a9cd30360dba9907116603f5068a9a9b69
Ronald Cardozo <ronaldcoyo@gmail.com>
2024-09-04 00:20:06 -0500

    Agregando campo a la tabla clientes

--- inventory/models.py
+++ inventory/models.py
@@ -5,6 +5,7 @@ class Cliente(models.Model):
     nombre = models.CharField(max_length=255)
     contacto = models.CharField(max_length=255)
     email = models.EmailField()
+    telefono = models.CharField(max_length=20)
 
 class Proveedor(models.Model):
     nombre = models.CharField(max_length=255)
